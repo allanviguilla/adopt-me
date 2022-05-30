b07fc2bd69c1e66dbe0045433dcc2ed3a42c60b9
allanviguilla <viguilla.allan@gmail.com>
2022-05-30 11:59:58 -0700

    error boundaries

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   //   constructor(props) {
@@ -25,6 +26,8 @@ class Details extends Component {
       return <h2>loading ...</h2>;
     }
 
+    throw new Error("LMAO why you do dat");
+
     const { animal, breed, city, state, description, name, images } =
       this.state;
 
@@ -46,7 +49,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />;
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
