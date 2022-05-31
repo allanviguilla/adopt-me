4290a2f12095a14176e235cfbc8390c5a4275fb7
allanviguilla <viguilla.allan@gmail.com>
2022-05-30 23:34:24 -0700

    context lecture and context for theming

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends Component {
   //   constructor(props) {
@@ -26,8 +27,6 @@ class Details extends Component {
       return <h2>loading ...</h2>;
     }
 
-    throw new Error("LMAO why you do dat");
-
     const { animal, breed, city, state, description, name, images } =
       this.state;
 
@@ -39,7 +38,11 @@ class Details extends Component {
           <h2>
             {animal} - {breed} - {city}, {state}
           </h2>
-          <button> Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}> Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
@@ -49,6 +52,7 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
+  //const [theme] = useContext(ThemeContext);
   return (
     <ErrorBoundary>
       <Details params={params} />;
